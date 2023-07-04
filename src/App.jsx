f59af2a42f7689961a560f259f55170e7fbaa303
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-04 21:49:09 +0100

    file structure created with the default files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import Detail from './Detail'
+import Home from './components/home/Home'
+import Detail from './components/detail/Detail'
+import MyNavbar from './components/header/MyNavbar'
+import MyFooter from './components/footer/MyFooter'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <MyNavbar/>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/detail" element={<Detail/>} />
     </Routes>
+    <MyFooter/>
   </BrowserRouter>
       
   )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       },[])
     return (
     <div>
-        Home
+        home
     </div>
     )
 }
