9529f404b4870b48c798bd871d8ef54133a30325
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-06 00:34:02 +0100

    productInterface added

---
+++ src/interface/productInterface.ts
@@ -0,0 +1,13 @@
+export interface Iproduct{
+        productName:String;
+        productId:String;
+        description:String;
+        categorgy:String[];
+        price:String;
+        offer:String;
+        brandName:String;
+        images:String[];
+        sizes:String[];
+        colors:String[];
+        rating:String
+}
