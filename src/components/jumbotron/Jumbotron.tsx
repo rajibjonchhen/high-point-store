065272c502052e21c0d18d52fdd4d5f1a22576e0
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-06 00:34:31 +0100

    random jumbotron added

--- src/components/jumbotron/Jumbotron.tsx
+++ src/components/jumbotron/Jumbotron.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './jumbotron.scss'
+import  Button from 'react-bootstrap/button';
 
 export default function Jumbotron() {
   return (
-    <div>Jumbotron</div>
+    <div className='container-fluid jumbotron'>
+    <h1>Hello, world!</h1>
+    <p>
+      This is a simple hero unit, a simple jumbotron-style component for calling
+      extra attention to featured content or information.
+    </p>
+    <p>
+      <Button variant="primary">Learn more</Button>
+    </p>
+  </div>
   )
 }
