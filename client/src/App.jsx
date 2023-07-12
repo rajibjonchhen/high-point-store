e001a5f6472f03c9ad370a6b8b91cd14276bdce9
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-12 23:21:42 +0100

    scroll to the top while page/path changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.scss'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './scences/home/Home'
 import Detail from './scences/detail/Detail'
 import MyNavbar from './scences/global/header/MyNavbar'
 import MyFooter from './scences/global/footer/MyFooter'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ConstRoutes } from './constant/ConstRoutes'
-
+import { useEffect } from 'react'
 function App() {
 
+  const ScrollToTop = () => {
+    const {pathname} = useLocation()
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname])
+  }
   return (
     <div className='app'>
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { theme } from './theme'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+            <App />
+    </ThemeProvider>
 )
