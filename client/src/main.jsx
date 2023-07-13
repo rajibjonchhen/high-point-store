621c5038956aaf9ceedb636dba1c030a6cca5407
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-14 00:01:09 +0100

    state setup in with Provider in main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,19 @@ import { theme } from './theme'
 import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'
-import { store } from './state/store.js'
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './state'
+
+const store  = configureStore({reducer:{
+    cart : cartReducer
+}})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+<Provider store = {store}>
    <ThemeProvider theme={theme}>
         <CssBaseline/>
             <App />
     </ThemeProvider>
+</Provider> 
   
 )
