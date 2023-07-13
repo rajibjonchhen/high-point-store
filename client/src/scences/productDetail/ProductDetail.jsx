5a2cc6a52ea6a62ab29bfa33ac853cb287eb2165
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-13 01:28:37 +0100

    product id obtained from param in detail page with useParam

--- client/src/scences/productDetail/ProductDetail.jsx
+++ client/src/scences/productDetail/ProductDetail.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react'
 import './productDetail.scss'
+import { useParams } from 'react-router-dom'
 
 export default function Detail() {
+  const param = useParams()  
+
+
     useEffect(()=>{
-        console.log("hello detail")
+       const productId = param.id
+       console.log(productId)
       },[])
   return (
     <div>Detail</div>
