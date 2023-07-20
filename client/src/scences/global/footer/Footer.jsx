791c7cd59566a0f9fddebe86536ef2233caa9d4a
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-20 23:48:26 +0100

    about the company

--- client/src/scences/global/footer/Footer.jsx
+++ client/src/scences/global/footer/Footer.jsx
@@ -8,7 +8,25 @@ export default function Footer() {
   const {palette : {neutral}} = useTheme()
   return (
     <Box mt='70px' p = '40px 0' backgroundColor = {neutral.light}>
+      <Box 
+        width = "80%"
+        margin = 'auto'
+        display = 'flex'
+        justifyContent = 'space-between'
+        flexWrap = 'wrap'
+        rowGap = '30px'
+        columnGap = 'clamp(20px 30px 40px)' 
+      >
+        <Box width = 'clamp(20% 30% 40%)'
+        >
+            <Typography variant='h4' fontWeight = 'bold' mb = '30px' color = {shades.secondary[500]}>High Point</Typography>
+            <div>
+            Risus primis platea egestas facilisi donec. Nam inceptos cubilia donec egestas ornare mattis. Scelerisque metus nullam per tempus pretium dis curabitur primis dictum taciti eget. Pede duis libero tellus rutrum bibendum sed gravida mattis scelerisque turpis molestie. Sagittis convallis litora metus rutrum senectus. Nisi ad condimentum semper diam habitant metus.
+            </div>
+        </Box>
      
+
+      </Box>
     </Box>
   )
 }
