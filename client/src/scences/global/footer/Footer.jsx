8f97a8d0d776eea47b63a4fdc92c6dd603fe792e
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-20 23:46:40 +0100

    footer skeleton

--- client/src/scences/global/footer/Footer.jsx
+++ client/src/scences/global/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Box, Typography, useTheme} from '@mui/material'
 import './footer.scss'
+import { shades } from '../../../theme'
 
 
 export default function Footer() {
+  const {palette : {neutral}} = useTheme()
   return (
-    <div>MyFooter</div>
+    <Box mt='70px' p = '40px 0' backgroundColor = {neutral.light}>
+     
+    </Box>
   )
 }
