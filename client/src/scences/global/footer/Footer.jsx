ee8545ae2ebeb4661556cc7349cdb4355a79d67a
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-21 23:14:09 +0100

    footer completed with four column

--- client/src/scences/global/footer/Footer.jsx
+++ client/src/scences/global/footer/Footer.jsx
@@ -15,32 +15,32 @@ export default function Footer() {
         justifyContent = 'space-between'
         flexWrap = 'wrap'
         rowGap = '30px'
-        columnGap = 'clamp(20px 30px 40px)' 
+        columnGap = 'clamp(20px, 30px, 40px)' 
       >
-        <Box width = 'clamp(20% 30% 40%)'>
-            <Typography variant='h4' fontWeight = 'bold' mb = '30px' color = {shades.secondary[500]}>High Point</Typography>
-            <p>
+        <Box width = 'clamp(20%, 30%, 40%)'>
+            <Typography variant='h4' fontWeight = 'bold' mb = '20px' color = {shades.secondary[500]}>High Point</Typography>
+            <div>
               Risus primis platea egestas facilisi donec. Nam inceptos cubilia donec egestas ornare mattis. Scelerisque metus nullam per tempus pretium dis curabitur primis dictum taciti eget. Pede duis libero tellus rutrum bibendum sed gravida mattis scelerisque turpis molestie. Sagittis convallis litora metus rutrum senectus. Nisi ad condimentum semper diam habitant metus.
-            </p>
+            </div>
         </Box>
         <Box>
-          <Typography fontWeight = 'bold' mb = '30px'  >About Us</Typography>
-          <Typography  mb = '30px' >Career</Typography>
-          <Typography  mb = '30px' >Terms and condition</Typography>
-          <Typography  mb = '30px' >Privacy Policy</Typography>
+          <Typography variant='h4'  fontWeight = 'bold' mb = '20px'  >About Us</Typography>
+          <Typography  mb = '20px' >Career</Typography>
+          <Typography  mb = '20px' >Terms and condition</Typography>
+          <Typography  mb = '20px' >Privacy Policy</Typography>
         </Box>
         <Box>
-          <Typography  mb = '30px' >Help Center</Typography>
-          <Typography  mb = '30px' >Track your order</Typography>
-          <Typography  mb = '30px' >Corporate and bulk purchasing</Typography>
-          <Typography  mb = '30px' >Returns and refund</Typography>
+          <Typography variant='h4'  fontWeight = 'bold' mb = '20px' >Help Center</Typography>
+          <Typography  mb = '20px' >Track your order</Typography>
+          <Typography  mb = '20px' >Corporate and bulk purchasing</Typography>
+          <Typography  mb = '20px' >Returns and refund</Typography>
         </Box>
-        <Box width = 'clamp(20% 25% 30%)'
+        <Box width = 'clamp(20%, 25%, 30%)'
         >
-            <Typography variant='h4' fontWeight = 'bold' mb = '30px' >Contact Us</Typography>
-            <Typography  mb = '30px' >Rua Langgade - 39 </Typography>
-            <Typography  mb = '30px' >Valby Langgade</Typography>
-            <Typography  mb = '30px' >2250</Typography>
+            <Typography variant='h4' fontWeight = 'bold' mb = '20px' >Contact Us</Typography>
+            <Typography  mb = '20px' >Rua Langgade - 39 </Typography>
+            <Typography  mb = '20px' >Valby Langgade</Typography>
+            <Typography  mb = '20px' >2250</Typography>
         </Box>
       </Box>
     </Box>
