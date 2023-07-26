bf1cd9d808dbc0df2bc4e28ffb6cef929510562d
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-26 23:59:08 +0100

    checkout page added

--- client/src/scences/checkOut/CheckOut.jsx
+++ client/src/scences/checkOut/CheckOut.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './checkOut.scss'
 import { ConstRoutes } from '../../constant/ConstRoutes'
-import { Link } from 'react-router-dom'
+import * as yup from 'yup'
+import {Formik} from 'formik'
+
+import {
+  Box,
+  IconButton,
+  Typography,
+  Button,
+  useTheme,
+  Stepper,
+  Step,
+  StepLabel
+} from "@mui/material";
+import { useSelector } from 'react-redux';
+import { shades } from '../../theme'
 
 export default function CheckOut() {
+  const cart = useSelector(state => state.cart.cart)
+const [activeStep,setActiveStep] = useState(0)
+let isFirstStep = activeStep === 0
+let isSecondStep = activeStep === 1
+
+const handleFormSubmit = async(value, action) =>  {
+  setActiveStep(activeStep+1)
+}
+
+async function makePayment(values){
+
+}
   return (
-    <div>
+    <Box width = '80%' m = '100px auto'>
         CheckOut
         <Link to = {ConstRoutes.confirmation.url()}>
             <button>Confirmation</button>
         </Link>
-    </div>
+    </Box>
   )
 }
