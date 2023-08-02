cb9a6ea37db9876a8b00db206bd097f8e9fa4f5a
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-08-02 11:44:13 +0100

    backend setup for stripe payment

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,48 +1,61 @@
-'use strict';
-const stripe  = require("stripe")(process.env.STRIPE_SECRET_KEY)
+"use strict";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order', ({strapi}) => ({
-  async create(ctx){
-    const {products, userName, email} = ctx.request.body
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
     try {
       // retrieve item information
       const lineItems = await Promise.all(
-        products.map(async(product) => {
-          const item = await strapi.service("api::item.item").findOne(product.id);
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
 
           return {
-            price_data : {
-              currency : "usd",
-              pruduct_data : {
-                name : item.name,
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
               },
-              unit_amount : item.price * 100,
+              unit_amount: item.price * 100,
             },
-            quantity : product.count
-          }
+            quantity: product.count,
+          };
         })
       );
+        console.log("line", lineItems)
+      // create a stripe session
       const session = await stripe.checkout.sessions.create({
-        payment_method_types : ['card'],
-        customer_email : email,
-        mode : "payment",
-        success_url : "http://localhost:3000/checkout/success",
-        cancel_url : "http://localhost:3000/checkout/cancel",
-        line_items : lineItems
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:5173/checkout/success",
+        cancel_url: "http://localhost:5173",
+        line_items: lineItems,
       });
-      //create the item
-      await strapi.service("api::order.order").create({
-        data : {userName, products, stripeSessionId : session.id}
-      })
-      return{ id : session.id}
+
+      // create the item
+      await strapi
+        .service("api::order.order")
+        .create({ data: { userName, products, stripeSessionId: session.id } });
+
+      // return the session id
+      return { id: session.id };
+      console.log("session")
     } catch (error) {
-      ctx.response.status = 500
-      return {error : {message : "There was a problem creating the change."}}
+      console.log(error)
+      ctx.response.status = 500;
+      return { error: {
+        message: "There was a problem creating the change",
+        err : error
+    } };
     }
-  }
+  },
 }));
